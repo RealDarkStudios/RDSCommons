0b09a8cf993cee5adf27529aadfc34ad4fe16d29
RealDarkStudios <jmorton@digitalunderworlds.com>
2024-03-29 15:11:10 -0500

    Snapshot 24w13h for 1.0.0.0
    Added:
    
    Removed:
    - IYamlStorage
    - IYamlStorageWithID
    - IYamlStorageWithIDAndObject
    
    Refactors:
    
    Changes:
    
    Took 6 minutes

--- src/main/java/net/realdarkstudios/commons/misc/IYamlSerializableEnum.java
+++ src/main/java/net/realdarkstudios/commons/misc/IYamlSerializableEnum.java
@@ -2,7 +2,7 @@ package net.realdarkstudios.commons.misc;
 
 import org.jetbrains.annotations.NotNull;
 
-public interface IYamlSerializableEnum<T> {
+public interface IYamlSerializableEnum {
     @NotNull
     String getId();
 }
